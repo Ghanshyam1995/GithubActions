983d4bdb5874ed80e6398bc3007ecaa9d8bbedf2
Ghanshyam Singh Garia <jgariag@jci.com>
2023-07-17 14:17:25 +0530

    added test project

---
+++ GithubAction.API/Services/IShoppingCartService.cs
@@ -0,0 +1,12 @@
+﻿using GithubAction.API.Entities;
+
+namespace GithubAction.API.Services
+{
+    public interface IShoppingCartService
+    {
+        IEnumerable<ShoppingItem> GetAllItems();
+        ShoppingItem Add(ShoppingItem newItem);
+        ShoppingItem GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
